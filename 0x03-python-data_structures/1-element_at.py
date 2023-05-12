f479f0a740e89bfb4705046a1c4049ec1aaab888
kenyapower <vincentn371@gmail.com>
2023-05-12 08:33:02 -0700

    task 1

---
+++ 0x03-python-data_structures/1-element_at.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def element_at(my_list, idx):
+    """
+    Gets an element fromm list using index
+    """
+    if idx >= len(my_list) or idx < 0:
+        return
+
+    return (my_list[idx])
